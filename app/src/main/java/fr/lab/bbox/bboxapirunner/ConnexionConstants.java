695c9cef3258ba9890d741cc4d3c6448ff8cce9d
bigotalex <alexandre.bigot9@gmail.com>
2017-01-18 15:19:57 +0100

    New Bbox ConnexionConstants

--- app/src/main/java/fr/lab/bbox/bboxapirunner/ConnexionConstants.java
+++ app/src/main/java/fr/lab/bbox/bboxapirunner/ConnexionConstants.java
@@ -5,11 +5,11 @@ package fr.lab.bbox.bboxapirunner;
  */
 public class ConnexionConstants {
 
-    public static String Org = "9mzepw";
-    public static String Type= "Android";
-    public static String Id="BBoxMiami";
-    public static String AuthMethod="token";
-    public static String AuthToken="(X(9AhG6q0+NIEZSt6";
+    public static String Org = "xdq8ny";
+    public static String Type = "Bbox";
+    public static String Id = "Bbox_Miami";
+    public static String AuthMethod = "token";
+    public static String AuthToken = "Xo5g-?lWT)I(9WY8+Y";
     public static String city = "";
 
 }
